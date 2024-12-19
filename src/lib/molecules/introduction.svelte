da966414e5e10958c1d21c44e1e2367a1dac13d1
Robin <robin.django@gmail.com>
2024-12-19 22:25:23 +0100

    Ported over from original repository

---
+++ src/lib/molecules/introduction.svelte
@@ -0,0 +1,33 @@
+<script>
+  export let data;
+
+  const {title, content} = data
+
+  let newTitle = title.replace(/a/g, 'A');
+</script>
+
+<header>
+  <h1>{newTitle}</h1>
+    
+  <!-- {@html content.html} -->
+</header>
+
+<style>
+  header {
+      max-width: 40rem;
+      margin: 0 auto 2rem;
+  }
+
+  h1 {
+      font-size: 1.7rem;
+  }
+  @media screen and (min-width: 36em) {
+      header {
+          width: 75%;
+      }
+      h1 {
+          font-size: 3.8rem;
+          text-align: center;
+      }
+  }
+</style>
